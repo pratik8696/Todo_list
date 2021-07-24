98b917b911182585b170cdbecb16df21a5c26a3b
pratik8696 <mepratikanand@gmail.com>
2021-07-24 21:49:27 +0530

    v.3.3

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.get("/about",function(req, res){
   res.render("about");
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is up and running");
 });
